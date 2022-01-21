0cf6ee22d721a440717c3920379dd563805983d7
rayantwp <wdasdfe@mail.ru>
2022-01-21 23:52:19 +0300

    fix formatting

--- js/script.js
+++ js/script.js
@@ -1,73 +1,73 @@
 function start(f) {
-	/in/.test(document.readyState) ? setTimeout('start(' + f + ')', 9) : f();
-  }
-  start(function() {
-var app = new Vue({
-	el: '#app',
+  /in/.test(document.readyState) ? setTimeout('start(' + f + ')', 9) : f();
+}
+start(function () {
+  var app = new Vue({
+    el: '#app',
     data() {
       return {
         valueName: '',
         valueService: '',
         valueEmail: '',
         valuePassword: '',
-		    default: 'В очереди',  
-    parents: [{
-			img: 'img/doggie3.jpg',
-			nickname:'Белла',
-			service: 'Педикюр',
-			status:'Готово!' ,	
-		  },
-        { 
-			img:'img/doggie2.jpg',
-			nickname:'Бонни',
-			service: 'Мытье',
-			status:'Готово!' ,
-        },
-        { 
-			img:'img/kitty2.jpg',
-			nickname:'Рокки',
-			service: 'Мытье',
-			status:'Готово!' ,
+        default: 'В очереди',
+        parents: [{
+          img: 'img/doggie3.jpg',
+          nickname: 'Белла',
+          service: 'Педикюр',
+          status: 'Готово!',
         },
         {
-			img:'img/doggie1.jpg',
-			nickname:'Атос',
-			service:'Стрижка',
-			status:'Готово!' ,
+          img: 'img/doggie2.jpg',
+          nickname: 'Бонни',
+          service: 'Мытье',
+          status: 'Готово!',
         },
         {
-			img:'img/kitty1.jpg',
-			nickname:'Матильда',
-			service: 'Стрижка',
-			status: 'Готово!'
+          img: 'img/kitty2.jpg',
+          nickname: 'Рокки',
+          service: 'Мытье',
+          status: 'Готово!',
+        },
+        {
+          img: 'img/doggie1.jpg',
+          nickname: 'Атос',
+          service: 'Стрижка',
+          status: 'Готово!',
+        },
+        {
+          img: 'img/kitty1.jpg',
+          nickname: 'Матильда',
+          service: 'Стрижка',
+          status: 'Готово!'
         }
-      ],
+        ],
       }
     },
     methods: {
-		inputName(event){
+      inputName(event) {
         this.valueName = event.target.value;
       },
-      inputService(event){
+      inputService(event) {
         this.valueService = event.target.value;
       },
-      submissionForm(){
-        if(this.valueName === '' && this.valueService === '') {
+      submissionForm() {
+        if (this.valueName === '' && this.valueService === '') {
           return
         };
         this.parents.push({
-		      nickname: this.valueName,
+          nickname: this.valueName,
           service: this.valueService,
           status: this.default
         });
       },
-      inputEmail(event){
+      inputEmail(event) {
         this.valueEmail = event.target.value;
       },
-      inputPassword(event){
+      inputPassword(event) {
         this.valuePassword = event.target.value;
       },
-      consoleConclusion(){
+      consoleConclusion() {
         console.log(`email: ${this.valueEmail} password: ${this.valuePassword}`);
       }
     }
